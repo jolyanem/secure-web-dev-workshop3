6a7eea0e9aac08e57f8fad3e7bd9aaba97d5ad3f
Jolyane MAKARISON MAC MIOU FO <jolyane.makarison@gmail.com>
2022-12-12 00:11:41 +0100

    find one location with an ID

--- locations/locations.controller.js
+++ locations/locations.controller.js
@@ -3,10 +3,25 @@
 
 const router = require('express').Router()
 const locationsService = require('./locations.service')
+const { default: mongoose } = require('mongoose')
+const Location = require('./locations.model')
 
+router.get('/', (req, res) => {
+	return res.status(200).send("Hello World")
+})
 router.get('/locations', async (req, res) => {
 	const allLocations = await locationsService.findAll();
 	res.json(allLocations)
+})
+
+router.get('/locations/:id', async(req,res) => {
+	try {
+		const location = await locationsService.findOne(req.params['id'])
+		return res.status(200).send(location)
+	} catch (e) {
+		if (e.message === "Location not found") return res.status(404).send("Location not found")
+		return res.status(400).send("Bad Request")
+	}
 })
 
 
--- locations/locations.service.js
+++ locations/locations.service.js
@@ -1,4 +1,4 @@
-// This file holds the Business-Logic layer, interacting with Data Layer
+
 
 const Location = require('./locations.model')
 
@@ -6,5 +6,15 @@ function findAll () {
 	return Location.find();
 }
 
+async function findOne(id){
+	const location = await Location.findById(id);
+	if(!location) throw new Error ("Location not found :(")
+	return location
+}
+
+
+
+
 module.exports.findAll = findAll
+module.exports.findOne = findOne
 
